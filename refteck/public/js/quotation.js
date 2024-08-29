5723385fca583bc164ef40bd30dd7bb672536954
Sakshi-Greycube <sakshi@greycube.in>
2024-08-29 12:09:53 +0530

    fix: sq details html field issue

--- refteck/public/js/quotation.js
+++ refteck/public/js/quotation.js
@@ -1,18 +1,41 @@
 frappe.ui.form.on("Quotation", {
     onload(frm) {
-        if (
-            frm.fields_dict["custom_previous_quotation_details"] &&
-            frm.is_new() == undefined &&
-            frm.doc.__onload && "custom_html_data" in frm.doc.__onload
-        ) {
-            frm.set_df_property('custom_previous_quotation_details', 'options', frm.doc.__onload.custom_html_data)
-        }
-        if (
-            frm.fields_dict["custom_sq_details"] &&
-            frm.is_new() == undefined &&
-            frm.doc.__onload && "custom_sq_html_data" in frm.doc.__onload
-        ) {
-            frm.set_df_property('custom_sq_details', 'options', frm.doc.__onload.custom_sq_html_data)
-        }
-    }
+        draw_html(frm)
+
+    },
+    refresh(frm) {
+        draw_html(frm)
+
+    },
+    setup(frm) {
+        draw_html(frm)
+
+    },    
 })
+
+
+function draw_html(frm) {
+    if (
+        frm.fields_dict["custom_previous_quotation_details"] &&
+        frm.is_new() == undefined &&
+        frm.doc.__onload && "custom_html_data" in frm.doc.__onload
+    ) {
+        frm.set_df_property('custom_previous_quotation_details', 'options', frm.doc.__onload.custom_html_data)
+        frm.refresh_field('custom_previous_quotation_details')
+    }else{
+        frm.set_df_property('custom_previous_quotation_details', 'options', "<div><div>")
+        frm.refresh_field('custom_previous_quotation_details')         
+    }
+    if (
+        frm.fields_dict["custom_sq_details"] &&
+        frm.doc.custom_margin_calculation.length>0 &&
+        frm.doc.__onload && "custom_sq_html_data" in frm.doc.__onload
+    ) {
+        frm.set_df_property('custom_sq_details', 'options', frm.doc.__onload.custom_sq_html_data)
+        frm.refresh_field('custom_sq_details')
+       
+    }else{
+        frm.set_df_property('custom_sq_details', 'options', '<div><div>')
+        frm.refresh_field('custom_sq_details')
+    }   
+}
